31dc20c107d6179ce66f46465e0e656ec8f339b7
tj-bu-706-19 <tj-bu-706-19@DESKTOP-ATIMVVC>
2025-03-20 18:19:53 +0900

    로또 근데 이상하게 품

--- CodeUp/src/_1226/Main.java
+++ CodeUp/src/_1226/Main.java
@@ -1,23 +1,63 @@
 package _1226;
 
-import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[7];
+		int a = 0;
+		int b = 0;
+		int[] arr = new int[6];
+		int bonus = (int)(Math.random()*50+1);
 		int[] arr2 = new int[6];
 		
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = (int)(Math.random()*50)+1;
 		}
 		System.out.println();
 		
 		for (int j = 0; j < arr2.length; j++) {
-			arr[j] = sc.nextInt();
+			arr2[j] = (int)(Math.random()*50)+1;
 		}
 		
+		for (int num : arr) {
+			System.out.print(num + " ");
+		}
+		System.out.print(bonus);
+		System.out.println();
+		for (int num : arr2) {
+			System.out.print(num + " ");
+			
+		}
 		
+		for (int k = 0; k < arr.length; k++) {
+			for (int k2 = 0; k2 < arr2.length; k2++) {
+				
+				if(arr[k] == arr2 [k2]) {
+					a++;
+				}
+			}
+			
+		}
+		for (int i = 0; i < arr2.length; i++) {
+			if(arr2[i] == bonus)
+				b = 1;
+		}
+		
+		System.out.println();
+		System.out.print(a);
+		System.out.println();
+		
+		if(a == 6)
+			System.out.print("1등");
+		else if(a == 5 && b == 1)
+			System.out.print("2등");
+		else if(a == 5)
+			System.out.print("3등");
+		else if(a == 4)
+			System.out.print("4등");
+		else if(a == 3)
+			System.out.print("5등");
+		else if(a == 2)
+			System.out.print("0등");
 		
 		
 	}
