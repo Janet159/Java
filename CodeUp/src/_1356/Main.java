5db7d5bf5d054345180a00b5757a07a967437138
tj-bu-706-19 <tj-bu-706-19@DESKTOP-ATIMVVC>
2025-03-21 17:18:25 +0900

    10일차

---
+++ CodeUp/src/_1356/Main.java
@@ -0,0 +1,24 @@
+package _1356;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for (int i = 1; i <= a; i++) {
+			System.out.print("*");
+		}
+
+		System.out.print("*");
+
+		System.out.print("*");
+		
+		
+		for (int k = 1; k <= a; k++) {
+			System.out.print("*");
+		}
+		
+	}
+}
