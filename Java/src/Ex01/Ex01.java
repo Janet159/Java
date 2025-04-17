0700633e7f0f7b16ef0552f8a14fdb77d782a157
Janet159 <janet15916@gmail.com>
2025-04-18 00:16:02 +0900

    dd

---
+++ Java/src/Ex01/Ex01.java
@@ -0,0 +1,43 @@
+package Ex01;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] a = new int[5];
+		
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < a.length-1; i++) {
+			for (int j = 0; j < a.length-1-i; j++) {
+				if (a[j]>a[j+1]) {
+					int swap = a[j];
+					a[j] = a[j+1];
+					a[j+1] = swap;
+				}
+			}
+		}
+		for (int i : a) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		
+		for (int i = 0; i < a.length-1; i++) {
+			for (int j = 0; j < a.length-1-i; j++) {
+				if (a[j]<a[j+1]) {
+					int swap = a[j];
+					a[j] = a[j+1];
+					a[j+1] = swap;
+				}
+			}
+		}
+		for (int i : a) {
+			System.out.print(i+" ");
+		}
+	
+}
+}
